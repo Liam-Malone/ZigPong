6b2a8c3ccf3fd83e92d45e61bcfb00149be46659
MediocreDev <liammaloneirlusa@gmail.com>
2023-08-27 13:31:38 +0100

    simplify collision, swap nums for consts

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,9 @@
 //TODO:
-// - Improve ball-to-paddle collision (vertical)
-// - Add score-counting and win/lose-conditions
 // - Add text rendering
+// - Improve ball-to-paddle collision:
+//   - collide on top/bottom of paddle
+//   - stop ball getting stuck in paddle
+// - Add score-counting and win/lose-conditions
 const std = @import("std");
 const c = @cImport({
     @cInclude("SDL2/SDL.h");
@@ -11,16 +13,21 @@ const overlaps = c.SDL_HasIntersection;
 const Color = enum{
     white,
 };
+const Player = enum{
+    player_one,
+    player_two,
+};
+
 const Paddle = struct {
     is_human: bool,
-    player_number: u8,
+    player: Player,
     x: f32,
     y: f32,
     height: f32,
     width: f32,
     dy: f32,
     color: Color,
-    score: u32,
+    score: u8,
     rect: c.SDL_Rect,
 };
 
@@ -39,6 +46,11 @@ const DELTA_TIME_SEC: f32 = 1.0/@as(f32, FPS);
 const WINDOW_WIDTH = 800;
 const WINDOW_HEIGHT = 600;
 const BACKGROUND_COLOR = 0xFF181818;
+const MAX_SCORE = 10;
+const MAX_PLAYER_SPEED = 3;
+const SPEED_INCREASE = 0.5;
+const PADDLE_HEIGHT = 60;
+const PADDLE_WIDTH = 20;
 
 fn make_rect(x: f32, y: f32, w: f32, h: f32) c.SDL_Rect {
     return c.SDL_Rect {
@@ -67,60 +79,71 @@ fn collide_vert_border(paddle: *Paddle) bool {
     return false;
 }
 
-fn collide(ball: *Ball, paddle: *Paddle) void {
-    if (overlaps(&ball.rect, &paddle.rect) != 0){
-        //if (ball.x + ball.dx)
-        switch(paddle.player_number){
-            1 => {
-                if (ball.x + ball.dx >= paddle.x - ball.size) {
-                    ball.dx *= -1;
-                }
-            },
-            2 => {
-                if (ball.x + ball.dx <= paddle.x + paddle.width){
-                    ball.dx *= -1;
-                }
-            },
-            else => unreachable,
-        }
+fn paddle_collide(ball: *Ball, paddle: *Paddle) void {
+    switch(paddle.player){
+        Player.player_one => {
+            ball.dx *= -1;
+        },
+        Player.player_two => {
+            ball.dx *= -1;
+        },
     }
 }
 
-fn update(ball: *Ball, player: *Paddle, computer: *Paddle) void{
-    if (collide_vert_border(computer)) {
-        computer.dy *= -1;
+fn win(score: u8) void {
+    if (score == MAX_SCORE) {
+        game_over = true;
+    }
+}
+
+
+fn update(ball: *Ball, player_1: *Paddle, player_2: *Paddle) void{
+    win(player_1.score);
+    win(player_2.score);
+    if (collide_vert_border(player_2)) {
+        player_2.dy *= -1;
     }
     ball.y += ball.dy;
-    _ = collide_vert_border(player);
+    _ = collide_vert_border(player_1);
 
 
-    if (overlaps(&ball.rect, &player.rect) != 0) {
-        ball.y = player.y - player.width/2 - ball.size - 1.0;
+    if (overlaps(&ball.rect, &player_1.rect) != 0) {
+        ball.y = player_1.y - player_1.width/2 - ball.size - 1.0;
     }
-    if (ball.x <= 0 or ball.x + ball.size >= WINDOW_WIDTH) {
+
+    if (ball.x + ball.size <= 0) {
         ball.x = WINDOW_WIDTH/2;
+        player_1.score += 1;
+    } else if (ball.x > WINDOW_WIDTH) {
+        ball.x = WINDOW_WIDTH/2;
+        player_2.score += 1;
     }
+
     if (ball.y + ball.dy <= 0 or ball.y + ball.dy >= WINDOW_HEIGHT + ball.size) {
         ball.dy *= -1;
     }
 
-    collide(ball, player);
-    collide(ball, computer);
+    if (overlaps(&ball.rect, &player_1.rect) != 0) {
+        paddle_collide(ball, player_1);
+    }
+    if (overlaps(&ball.rect, &player_2.rect) != 0){
+        paddle_collide(ball, player_2);
+    }
 
     ball.x += ball.dx;
-    player.y += player.dy;
-    computer.y += computer.dy;
+    player_1.y += player_1.dy;
+    player_2.y += player_2.dy;
 }
 
 
-fn render(renderer: *c.SDL_Renderer, ball: Ball, player: Paddle, comp: Paddle) void {
+fn render(renderer: *c.SDL_Renderer, ball: Ball, player_1: Paddle, comp: Paddle) void {
     //set_color(renderer, 
     var local_color: u32 = 0xFFFFFFFF;
     set_color(renderer, local_color);
     _ = c.SDL_RenderFillRect(renderer, &ball.rect);
 
     set_color(renderer, local_color);
-    _ = c.SDL_RenderFillRect(renderer, &player.rect);
+    _ = c.SDL_RenderFillRect(renderer, &player_1.rect);
 
     set_color(renderer, local_color);
     _ = c.SDL_RenderFillRect(renderer, &comp.rect);
@@ -129,34 +152,36 @@ fn render(renderer: *c.SDL_Renderer, ball: Ball, player: Paddle, comp: Paddle) v
 var quit = false;
 var started = false;
 var pause = false;
+var game_over = false;
+
 pub fn main() !void {
-    var player = Paddle{
+    var player_1 = Paddle{
         .is_human = true,
-        .player_number = 1,
+        .player = Player.player_one,
         .x = WINDOW_WIDTH - 60,
         .y = WINDOW_HEIGHT/2,
-        .height = 60,
-        .width = 20,
+        .height = PADDLE_HEIGHT,
+        .width = PADDLE_WIDTH,
         .dy = 0,
         .color = Color.white,
         .score = 0,
         .rect = undefined,
     };
-    player.rect = make_rect(player.x, player.y, player.width, player.height);
+    player_1.rect = make_rect(player_1.x, player_1.y, player_1.width, player_1.height);
 
-    var computer = Paddle{
+    var player_2 = Paddle{
         .is_human = false,
-        .player_number = 2,
+        .player = Player.player_two,
         .x = 30,
         .y = WINDOW_HEIGHT/2,
-        .height = 60,
-        .width = 20,
+        .height = PADDLE_HEIGHT,
+        .width = PADDLE_WIDTH,
         .dy = 0,
         .color = Color.white,
         .score = 0,
         .rect = undefined,
     };
-    computer.rect = make_rect(computer.x, computer.y, computer.width, computer.height);
+    player_2.rect = make_rect(player_2.x, player_2.y, player_2.width, player_2.height);
 
     var ball = Ball{
         .x = WINDOW_HEIGHT/2,
@@ -188,7 +213,7 @@ pub fn main() !void {
     defer c.SDL_DestroyRenderer(renderer);
 
     const keyboard = c.SDL_GetKeyboardState(null);
-    while (!quit) {
+    while (!quit and !game_over) {
         var event: c.SDL_Event = undefined;
         while (c.SDL_PollEvent(&event) != 0) {
             switch (event.@"type") {
@@ -203,39 +228,43 @@ pub fn main() !void {
             }
         }
          if (keyboard[c.SDL_SCANCODE_UP] != 0) {
-            player.dy += -0.5;
+            if ((player_1.dy * -1) < MAX_PLAYER_SPEED) {
+                player_1.dy += (SPEED_INCREASE * -1);
+            }
             if (!started) {
                 started = true;
-                player.dy = -1;
+                player_1.dy = -1;
                 ball.dy = 2;
                 ball.dx = 2;
-                computer.dy = 5;
+                player_2.dy = 3;
             }
         }
         if (keyboard[c.SDL_SCANCODE_DOWN] != 0) {
-            player.dy += 0.5;
+            if (player_1.dy < MAX_PLAYER_SPEED) {
+                player_1.dy += SPEED_INCREASE;
+            }
             if (!started) {
                 started = true;
-                player.dy = 1;
+                player_1.dy = 1;
                 ball.dy = 2;
                 ball.dx = 2;
-                computer.dy = 5;
+                player_2.dy = 3;
             }
         }
         
         set_color(renderer, BACKGROUND_COLOR);
         _ = c.SDL_RenderClear(renderer);
 
-        render(renderer, ball, player, computer);
+        render(renderer, ball, player_1, player_2);
 
         c.SDL_RenderPresent(renderer);
         c.SDL_Delay(1000/FPS);
 
-        update(&ball, &player, &computer);
+        update(&ball, &player_1, &player_2);
         
         ball.rect = make_rect(ball.x, ball.y, ball.size, ball.size);
-        player.rect = make_rect(player.x, player.y, player.width, player.height);
-        computer.rect = make_rect(computer.x, computer.y, computer.width, computer.height);
+        player_1.rect = make_rect(player_1.x, player_1.y, player_1.width, player_1.height);
+        player_2.rect = make_rect(player_2.x, player_2.y, player_2.width, player_2.height);
     }
     
 }
