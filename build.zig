d0421e2da5c84edd7de729eb26189c072c2c9b8b
MediocreDev <liammaloneirlusa@gmail.com>
2023-12-25 15:58:42 +0000

    rewrite in Raylib

--- build.zig
+++ build.zig
@@ -1,116 +1,45 @@
 const std = @import("std");
+const ray = @import("lib/raylib-5.0/src/build.zig");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
 pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
 
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
-        .name = "ZigPong",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
+        .name = "pong",
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
 
-    if (exe.target.isDarwin() and exe.target.isNative()) {
-        // assumes you used brew to install sdl2 and sdl2_ttf
-        exe.addIncludePath(
-            .{ .path = "/usr/local/include" },
-        );
-        exe.addLibraryPath(
-            .{ .path = "/usr/local/lib" },
-        );
-    } else if (exe.target.isWindows()) {
-        exe.addIncludePath(
-            .{ .path = "vendor/windows/SDL2/x86_64-w64-mingw32/include" },
-        );
-        exe.addLibraryPath(
-            .{ .path = "vendor/windows/SDL2/x86_64-w64-mingw32/lib" },
-        );
-        if (!exe.target.isNative()) {
-            exe.linkSystemLibrary("opengl32");
-            exe.linkSystemLibrary("winmm");
-            exe.linkSystemLibrary("ole32");
-            exe.linkSystemLibrary("oleaut32");
-            exe.linkSystemLibrary("gdi32");
-            exe.linkSystemLibrary("setupapi");
-            exe.linkSystemLibrary("imm32");
-            exe.linkSystemLibrary("version");
-            exe.linkSystemLibrary("rpcrt4");
-        }
-    } else {
-        exe.addIncludePath(
-            .{ .path = "/usr/include" },
-        );
-        exe.addLibraryPath(
-            .{ .path = "/usr/lib" },
-        );
-        exe.addIncludePath(
-            .{ .path = "/usr/local/include" },
-        );
-        exe.addLibraryPath(
-            .{ .path = "/usr/local/lib" },
-        );
-    }
+    var rl = ray.addRaylib(b, target, optimize, .{});
+    exe.addIncludePath(.{ .path = "libs/raylib-5.0/src" });
+    exe.linkLibrary(rl);
 
-    exe.linkSystemLibrary("c");
-
-    exe.linkSystemLibrary("SDL2");
-    exe.linkSystemLibrary("SDL2_ttf");
-
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
     b.installArtifact(exe);
 
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
     const run_cmd = b.addRunArtifact(exe);
 
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
     run_cmd.step.dependOn(b.getInstallStep());
 
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
 
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
     const unit_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
 
+    unit_tests.addIncludePath(.{ .path = "libs/raylib-5.0/src" });
+    unit_tests.linkLibrary(rl);
     const run_unit_tests = b.addRunArtifact(unit_tests);
 
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_unit_tests.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,283 +1,295 @@
+const rl = @import("rl.zig");
 const std = @import("std");
-const lib = @import("lib.zig");
-const c = @import("c.zig");
-// syntax simplification for using imports
-const overlaps = c.SDL_HasIntersection;
-const allocator = std.heap.page_allocator;
+const builtin = @import("builtin");
 
-// Enum types
-const Color = lib.Color;
-const Player = lib.Player;
-// Structs
-const Paddle = lib.Paddle;
-const Ball = lib.Ball;
-const ScreenText = lib.ScreenText;
-const Window = lib.Window;
+pub const EXIT_KEY: c_int = switch (builtin.mode) {
+    .Debug => rl.KEY_Q,
+    else => rl.KEY_NULL,
+};
 
-// CONSTANTS
+// BEGIN ENUMS
+const Color = enum(u32) {
+    white = 0xFFFFFFFF,
+    purple = 0x7BF967AA,
+    red = 0xFC1A17CC,
+    dark_gray = 0x18181822,
+    blue = 0x0000CCFF,
+    green = 0x00AA0022,
+    void = 0xFF00FFFF,
+
+    pub fn make_rl_color(col: Color) rl.Color {
+        var color = @intFromEnum(col);
+        const r: u8 = @truncate((color >> (3 * 8)) & 0xFF);
+        const g: u8 = @truncate((color >> (2 * 8)) & 0xFF);
+        const b: u8 = @truncate((color >> (1 * 8)) & 0xFF);
+        const a: u8 = @truncate((color >> (0 * 8)) & 0xFF);
+
+        return rl.Color{
+            .r = r,
+            .g = g,
+            .b = b,
+            .a = a,
+        };
+    }
+};
+
+const Direction = enum {
+    Up,
+    Down,
+    Left,
+    Right,
+    None,
+};
+
+const GameStage = enum {
+    Menu,
+    Play,
+    Pause,
+    Over,
+};
+
+const PaddleID = enum {
+    PlayerOne,
+    PlayerTwo,
+};
+
+const PaddleType = enum {
+    Human,
+    AI,
+};
+
+// END ENUMS
+
+// BEGIN STRUCTS
+const Ball = struct {
+    pos: rl.Vector2 = .{ .x = 0, .y = 0 },
+    dx: f32 = 0,
+    dy: f32 = 0,
+    radius: f32 = BALL_RADIUS,
+};
+
+const Paddle = struct {
+    id: PaddleID,
+    p_type: PaddleType = .AI,
+    pos: rl.Vector2 = .{ .x = 0, .y = 0 },
+    w: f32 = 20,
+    h: f32 = 60,
+    dy: f32 = 0,
+    dir: Direction = .Down,
+};
+// END STRUCTS
+
+// BEGIN CONSTANTS
 const FPS = 60;
-const DELTA_TIME_SEC: f32 = 1.0 / @as(f32, FPS);
-const WINDOW_WIDTH = 800;
-const WINDOW_HEIGHT = 600;
-const BACKGROUND_COLOR = Color.dark_gray;
 const MAX_SCORE = 10;
-const MAX_PLAYER_SPEED = 4;
-const SPEED_INCREASE = 0.5;
-const BALL_SIZE = 8;
-const PADDLE_HEIGHT = 60;
-const PADDLE_WIDTH = 20;
+const PADDLE_SPEED = 500;
+const BALL_SPEED = 600;
+const BALL_RADIUS = 8;
+const PADDLE_HEIGHT = 80;
+const PADDLE_WIDTH = 10;
 
-fn set_render_color(renderer: *c.SDL_Renderer, col: c.SDL_Color) void {
-    _ = c.SDL_SetRenderDrawColor(renderer, col.r, col.g, col.b, col.a);
-}
-
-fn collide_vert_border(paddle: *Paddle) void {
-    if ((paddle.y + paddle.dy >= WINDOW_HEIGHT - paddle.height and paddle.dy > 0) or (paddle.y + paddle.dy <= 0 and paddle.dy < 0)) {
-        paddle.dy = 0;
-        return;
-    }
-}
-
-fn paddle_collide(ball: *Ball, paddle: *Paddle) void {
-    switch (paddle.player) {
-        Player.player_one => {
-            if (ball.x + ball.size > paddle.x) {
-                ball.dy *= -1;
-                ball.dy += paddle.dy * 0.5;
-            } else {
-                ball.dx *= -1;
-                ball.dy += paddle.dy * 0.5;
-            }
-        },
-        Player.player_two => {
-            if (ball.x < paddle.x) {
-                ball.dy *= -1;
-                ball.dy += paddle.dy * 0.5;
-            } else {
-                ball.dx *= -1;
-                ball.dy += paddle.dy * 0.5;
-            }
-        },
-    }
-}
-
-fn win(score: i32, player: Player) void {
-    if (score == MAX_SCORE) {
-        winner = player;
-        game_over = true;
-    }
-}
-
-fn pause(ball: *Ball, player_1: *Paddle, player_2: *Paddle) void {
-    ball.pause();
-    player_1.pause();
-    player_2.pause();
-    paused = true;
-}
-
-fn unpause(ball: *Ball, player_1: *Paddle, player_2: *Paddle) !void {
-    try ball.unpause();
-    player_1.unpause();
-    player_2.unpause();
-    paused = false;
-}
-
-fn update(ball: *Ball, player_1: *Paddle, player_2: *Paddle) !void {
-    win(player_1.score, player_1.player);
-    win(player_2.score, player_2.player);
-
-    if (!player_2.is_human) {
-        player_2.move_to_ball(ball.*);
-    }
-    collide_vert_border(player_2);
-    collide_vert_border(player_1);
-
-    if (ball.x + ball.size <= 0) {
-        try player_1.update_score(1);
-        started = false;
-        ball.reset();
-        player_1.reset_pos();
-        player_2.reset_pos();
-        pause(ball, player_1, player_2);
-        return;
-    } else if (ball.x > WINDOW_WIDTH) {
-        try player_2.update_score(1);
-        started = false;
-        ball.reset();
-        player_1.reset_pos();
-        player_2.reset_pos();
-        pause(ball, player_1, player_2);
-        return;
-    }
-
-    if (ball.y + ball.dy <= 0 or ball.y + ball.dy >= WINDOW_HEIGHT + ball.size) {
-        ball.dy *= -1;
-    }
-
-    if (overlaps(&ball.next_rect(), &player_1.next_rect()) != 0) {
-        paddle_collide(ball, player_1);
-    }
-    if (overlaps(&ball.next_rect(), &player_2.next_rect()) != 0) {
-        paddle_collide(ball, player_2);
-    }
-
-    ball.update();
-    player_1.update();
-    player_2.update();
-}
-
-fn render(renderer: *c.SDL_Renderer, ball: Ball, player_1: Paddle, player_2: Paddle) void {
-    set_render_color(renderer, Color.make_sdl_color(ball.color));
-    _ = c.SDL_RenderFillRect(renderer, &ball.rect);
-
-    set_render_color(renderer, Color.make_sdl_color(player_1.color));
-    _ = c.SDL_RenderFillRect(renderer, &player_1.rect);
-
-    set_render_color(renderer, Color.make_sdl_color(player_2.color));
-    _ = c.SDL_RenderFillRect(renderer, &player_2.rect);
-}
-
-var quit = false;
-var paused = true;
-var started = false;
-var game_over = false;
-var winner: Player = undefined;
+// END CONSTANTS
 
 pub fn main() !void {
-    var window = try Window.init("ZigPong", 100, 100, WINDOW_WIDTH, WINDOW_HEIGHT);
-    defer window.deinit();
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var alloc = gpa.allocator();
 
-    var other_text: ScreenText = try ScreenText.init(WINDOW_WIDTH / 2 - 100, WINDOW_HEIGHT / 2, 30, Color.white, "ALT Text", window.renderer);
-    defer other_text.deinit();
+    const window_scale = 80;
+    const screen_width = 16 * window_scale;
+    const screen_height = 10 * window_scale;
 
-    var p1_score_msg: ScreenText = try ScreenText.init(
-        80,
-        20,
-        30,
-        Color.purple,
-        "P1 Score: 0",
-        window.renderer,
-    );
-    defer p1_score_msg.deinit();
+    var paddles: [2]Paddle = .{
+        .{
+            .id = .PlayerOne,
+            .pos = .{ .x = 20, .y = screen_height / 2 },
+        },
+        .{
+            .id = .PlayerTwo,
+            .pos = .{ .x = screen_width - (20 + 60), .y = screen_height / 2 },
+        },
+    };
+    var scores: [2]u8 = [2]u8{ 0, 0 };
 
-    var p2_score_msg: ScreenText = try ScreenText.init(
-        80,
-        60,
-        30,
-        Color.red,
-        "P2 Score: 0",
-        window.renderer,
-    );
-    defer p2_score_msg.deinit();
+    var ball: Ball = .{
+        .pos = .{
+            .x = (screen_width / 2),
+            .y = (screen_width / 2),
+        },
+    };
 
-    var player_1 = Paddle.init(
-        true,
-        Player.player_one,
-        WINDOW_WIDTH - 60,
-        WINDOW_HEIGHT / 2,
-        PADDLE_WIDTH,
-        PADDLE_HEIGHT,
-        Color.purple,
-        MAX_PLAYER_SPEED,
-        WINDOW_HEIGHT - PADDLE_HEIGHT,
-    );
-    var player_2 = Paddle.init(
-        false,
-        Player.player_two,
-        30,
-        WINDOW_HEIGHT / 2,
-        PADDLE_WIDTH,
-        PADDLE_HEIGHT,
-        Color.red,
-        MAX_PLAYER_SPEED,
-        WINDOW_HEIGHT - PADDLE_HEIGHT,
-    );
-    var ball = Ball.init(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, BALL_SIZE, Color.white);
+    var camera = rl.Camera2D{
+        .zoom = 0.75,
+        .offset = rl.Vector2{ .x = screen_width / 2, .y = screen_height / 2 },
+        .rotation = 0,
+        .target = .{
+            .x = (screen_width / 2),
+            .y = (screen_width / 2),
+        },
+    };
+    _ = camera;
+    var game_stage: GameStage = .Menu;
 
-    const keyboard = c.SDL_GetKeyboardState(null);
-    while (!quit) {
-        var event: c.SDL_Event = undefined;
-        while (c.SDL_PollEvent(&event) != 0) {
-            switch (event.type) {
-                c.SDL_QUIT => {
-                    quit = true;
-                },
-                c.SDL_KEYDOWN => switch (event.key.keysym.sym) {
-                    ' ' => {
-                        if (game_over) {
-                            player_1.reset();
-                            player_2.reset();
-                            game_over = false;
-                            continue;
-                        }
-                        if (!started) {
-                            started = true;
-                        }
-                        if (paused) {
-                            try unpause(&ball, &player_1, &player_2);
-                        } else if (!paused) {
-                            pause(&ball, &player_1, &player_2);
-                        }
-                    },
-                    'q' => {
-                        if (paused or game_over) {
-                            ball.reset();
-                            player_1.reset();
-                            player_2.reset();
-                            quit = true;
-                        }
-                    },
-                    else => {},
-                },
-                else => {},
-            }
+    rl.InitWindow(screen_width, screen_height, "platformer");
+    defer rl.CloseWindow();
+
+    rl.SetTargetFPS(60);
+    rl.SetExitKey(EXIT_KEY);
+
+    while (!rl.WindowShouldClose()) {
+        const delta_time: f32 = rl.GetFrameTime();
+        // UPDATE VARIABLES
+        switch (game_stage) {
+            .Menu => {
+                if (rl.IsKeyPressed(rl.KEY_ONE)) {
+                    paddles[0].p_type = .Human;
+                    game_stage = .Play;
+                } else if (rl.IsKeyPressed(rl.KEY_TWO)) {
+                    paddles[1].p_type = .Human;
+                    game_stage = .Play;
+                } else if (rl.GetKeyPressed() != 0) {
+                    game_stage = .Play;
+                }
+                ball.dy = BALL_SPEED / 10;
+                ball.dx = BALL_SPEED / 3;
+                for (paddles, 0..) |p, i| {
+                    if (p.p_type == .AI) paddles[i].dy = PADDLE_SPEED * delta_time;
+                }
+                // opening menu
+            },
+            .Play => {
+                // play game
+                if (rl.IsKeyPressed(rl.KEY_SPACE)) game_stage = .Pause;
+                update_paddles(&paddles, &ball, screen_height, delta_time);
+                update_ball(&ball, &paddles, &scores, screen_width, screen_height, delta_time);
+            },
+            .Pause => {
+                // pause game
+                if (rl.IsKeyPressed(rl.KEY_SPACE)) game_stage = .Play;
+                // handle clicking of UI elements
+            },
+            .Over => {
+                // game over screen
+                if (rl.GetKeyPressed() != 0) game_stage = .Play;
+            },
         }
-        if (!game_over) {
-            if (keyboard[c.SDL_SCANCODE_UP] != 0) {
-                if (!paused and (player_1.dy * -1) < MAX_PLAYER_SPEED) {
-                    player_1.dy += (SPEED_INCREASE * -1);
+        // DRAW
+        rl.BeginDrawing();
+        defer rl.EndDrawing();
+
+        rl.ClearBackground(rl.BLACK);
+        switch (game_stage) {
+            .Menu => {
+                rl.DrawText("Press 1 Or 2 To Select Player", screen_width / 5 * 2, (screen_height / 8) * 3, 24, rl.RAYWHITE);
+                rl.DrawText("(Or Any Other Key To Let It Play Itself)", screen_width / 10 * 3, (screen_height / 8) * 7, 24, rl.RAYWHITE);
+            },
+            .Play => {
+                //rl.BeginMode2D(camera);
+                for (paddles) |p| {
+                    rl.DrawRectangleRec(.{ .x = p.pos.x, .y = p.pos.y, .width = p.w, .height = p.h }, rl.RAYWHITE);
                 }
-            }
-            if (keyboard[c.SDL_SCANCODE_DOWN] != 0) {
-                if (!paused and player_1.dy < MAX_PLAYER_SPEED) {
-                    player_1.dy += SPEED_INCREASE;
-                }
-            }
+                rl.DrawCircleV(ball.pos, ball.radius, rl.RAYWHITE);
+                //rl.EndMode2D();
 
-            set_render_color(window.renderer, Color.make_sdl_color(BACKGROUND_COLOR));
-            _ = c.SDL_RenderClear(window.renderer);
-            if (paused and started) {
-                try other_text.render(window.renderer, "GAME PAUSED");
-            }
-
-            // update player scores
-            try p1_score_msg.update(window.renderer, allocator, @intFromEnum(player_1.player), player_1.score);
-            try p2_score_msg.update(window.renderer, allocator, @intFromEnum(player_2.player), player_2.score);
-
-            render(window.renderer, ball, player_1, player_2);
-
-            c.SDL_RenderPresent(window.renderer);
-            c.SDL_Delay(1000 / FPS);
-
-            try update(&ball, &player_1, &player_2);
-        } else {
-            started = false;
-            if (keyboard[c.SDL_SCANCODE_Q] != 0) {
-                ball.reset();
-                player_1.reset();
-                player_2.reset();
-                quit = true;
-            }
-            _ = c.SDL_RenderClear(window.renderer);
-            set_render_color(window.renderer, Color.make_sdl_color(BACKGROUND_COLOR));
-            _ = c.SDL_RenderClear(window.renderer);
-            if (winner == Player.player_one) {
-                try other_text.render(window.renderer, "PLAYER 1 WINS!!!");
-            } else {
-                try other_text.render(window.renderer, "PLAYER 2 WINS!!!");
-            }
-            c.SDL_RenderPresent(window.renderer);
-            c.SDL_Delay(1000 / FPS);
+                var tmp = std.fmt.allocPrint(alloc, "{d} | {d} ", .{ scores[1], scores[0] }) catch "err";
+                defer alloc.free(tmp);
+                @constCast(tmp)[tmp.len - 1] = 0;
+                const scores_str = tmp[0 .. tmp.len - 1 :0];
+                rl.DrawText(scores_str, screen_width / 2 - 30, 80, 30, rl.RAYWHITE);
+                rl.DrawFPS(40, 40);
+            },
+            .Pause => {
+                // Draw Pause UI
+                rl.DrawText("Game Paused", screen_width / 5 * 2, screen_height / 2, 40, rl.RAYWHITE);
+            },
+            .Over => {
+                // game over screen and UI
+            },
         }
     }
+}
+
+fn update_paddles(p_arr: []Paddle, ball: *Ball, screen_height: f32, delta_time: f32) void {
+    for (p_arr, 0..) |p, i| {
+        switch (p.p_type) {
+            .Human => {
+                if (rl.IsKeyDown(rl.KEY_UP)) {
+                    if (p.pos.y + (PADDLE_SPEED * -1) * delta_time <= 0) {
+                        p_arr[i].pos.y = 0;
+                    } else {
+                        p_arr[i].pos.y += (PADDLE_SPEED * -1) * delta_time;
+                    }
+                }
+                if (rl.IsKeyDown(rl.KEY_DOWN)) {
+                    p_arr[i].dir = .Down;
+                    if (p.pos.y + p.h + PADDLE_SPEED * delta_time >= screen_height) {
+                        p_arr[i].pos.y = screen_height - p.h;
+                    } else {
+                        p_arr[i].pos.y += PADDLE_SPEED * delta_time;
+                    }
+                }
+            },
+            .AI => {
+                const y_delta = ball.pos.y - (p.pos.y + (p.h / 2));
+                if (y_delta > 10 or y_delta < -10) {
+                    p_arr[i].dir = if (y_delta > 0) .Down else if (y_delta < 0) .Up else .None;
+                    switch (p_arr[i].dir) {
+                        .Up => {
+                            if (p.pos.y <= 0) {
+                                p_arr[i].pos.y = 0;
+                                p_arr[i].dir = .Down;
+                            } else {
+                                p_arr[i].pos.y += (PADDLE_SPEED / 4 * -1) * delta_time;
+                            }
+                        },
+                        .Down => {
+                            if (p.pos.y + p.h + PADDLE_SPEED * delta_time >= screen_height) {
+                                p_arr[i].pos.y = screen_height - p.h;
+                                p_arr[i].dir = .Up;
+                            } else {
+                                p_arr[i].pos.y += PADDLE_SPEED / 4 * delta_time;
+                            }
+                        },
+                        else => {}, // do nothing
+                    }
+                }
+            },
+        }
+    }
+}
+
+fn update_ball(b: *Ball, p_arr: []Paddle, scores: []u8, screen_width: f32, screen_height: f32, delta_time: f32) void {
+    if (b.pos.x > screen_width) {
+        b.pos.x = screen_width / 2;
+        scores[0] += 1;
+        return;
+    } else if (b.pos.x < 0) {
+        b.pos.x = screen_width / 2;
+        scores[1] += 1;
+        return;
+    }
+    // check for collision
+    if (b.pos.y <= 0 or b.pos.y + b.radius >= screen_height) {
+        b.pos.y -= b.dy * delta_time;
+        b.dy *= -1;
+        return;
+    }
+
+    for (p_arr) |p| {
+        if (p.pos.x <= b.pos.x + b.radius + b.dx * delta_time and
+            p.pos.x + p.w >= b.pos.x + b.dx * delta_time and
+            p.pos.y + p.h >= b.pos.y - b.radius + b.dx * delta_time and
+            p.pos.y <= b.pos.y + b.dx * delta_time)
+        {
+            b.pos.x = switch (p.id) {
+                .PlayerTwo => p.pos.x - b.radius,
+                .PlayerOne => p.pos.x + p.w + b.radius,
+            };
+            b.dx *= -1;
+            b.dy += p.dy * delta_time;
+            return;
+        }
+    }
+
+    b.pos.x += b.dx * delta_time;
+    b.pos.y += b.dy * delta_time;
 }
